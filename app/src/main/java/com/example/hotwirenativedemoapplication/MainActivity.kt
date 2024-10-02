dc70481e4070c3fa6cf2b3ffae0613ca11c238e3
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2024-10-01 23:46:28 -0400

    Updating activity_main layout and MainActivity Kotlin class code

--- app/src/main/java/com/example/hotwirenativedemoapplication/MainActivity.kt
+++ app/src/main/java/com/example/hotwirenativedemoapplication/MainActivity.kt
@@ -1,20 +1,20 @@
 package com.example.hotwirenativedemoapplication
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import dev.hotwire.navigation.activities.HotwireActivity
+import dev.hotwire.navigation.navigator.NavigatorConfiguration
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : HotwireActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
+
+    override fun navigatorConfigurations() = listOf(
+        NavigatorConfiguration(
+            name = "main",
+            startLocation = "https://hotwire-native-demo.dev",
+            navigatorHostId = R.id.main_nav_host
+        )
+    )
 }
